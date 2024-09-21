b7941ed806000990a92a66313caacea362fd9287
mugurel-giurcanu <123599217+mugurel-giurcanu@users.noreply.github.com>
2024-09-21 22:26:59 +0300

    change the initialalization procedure

--- SingletonApp/SingletonApp.cpp
+++ SingletonApp/SingletonApp.cpp
@@ -12,7 +12,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-
     CWinApp winApp(hInstance);
 
     if (!winApp.InitApp())
